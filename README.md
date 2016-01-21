acc6571b8022b8beb82ba0dfb5d5608a3ce2d396
Shalin Varghese <shalin.varghese@ugamsolutions.com>
2016-01-21 22:20:34 +0530

    Updated Read me and added comments

--- README.md
+++ README.md
@@ -9,10 +9,49 @@ Classifies each question into one of the following categories:
 -Affirmation
 -Unknown
 
+##Training Data Set:
 Trained on close to 6000 questions obtained majorly from UIUC dataset - http://cogcomp.cs.illinois.edu/Data/QA/QC/train_5500.label
 
-Model used - Logistic Regression
+##Features Used:
+###1. Syntatic Features:
+####- First Word Token : Which is the first word in the sentence ?
+####- First Word Type : Which Part-of-Speech does the first word belong to?
+####- First Noun : What is the first Noun present in the sentence ?
+####- First Question Token - What is the first question token ('Wh' word) present in the sentence ?
+####- First Question Noun - What is the first noun succeeding the first question token ?
+####- Is Affirmation - Is this question an affirmation question or not based on specific pattern rules ?
+
+###2. Ngram Features:
+####- Unigram
+####- Bigram
+
+###Is Affirmation rule set :
+
+####It is an affirmation question if - 
+
+Does it start with: 
+1. Be-verbs: = { am, is, are, been, being, was, were }.
+2. Modal-verbs: = {can, could, shall, should, will, would, may, might }.
+3. Auxiliary-verbs: = { do, did, does, have, had, has }.
+eg : “Is Obama the president of the U.S. now?” 
+
+####It is not an affirmation question if - 
+
+Does it start with : {'can', 'could','will', 'would','may'} and contain a 'wh' question later
+eg : "Can you tell me what is the time now?"
+
+Does it start with : Be-verbs and contain an 'or' later
+eg:“Is he married or not?” ,“Will the concern be on May 23rd or June 1st?”
+
+Does it contain the following pattern : 'some text' 'anybody'|'anyone' 'some text' 'tell'|'know' 'some text'
+eg:“Can anybody tell me who is the president of the U.S.?”, “Does anyone know how much Bill Gates earns a year?”
+
+
+Model used - Multi-class Logistic Regression
 
 Run the question_classifier.py file to for classification
 
-To train a new model, the train-config file can be modified. 
+To train a new model, the train-config file can be modified and run the 'training_man.py' file.
+
+Major dependencies are listed down in requirements.txt
+
--- question.py
+++ question.py
@@ -12,12 +12,29 @@ from sklearn.externals import joblib
 import pdb
 import nltk
 import time
-class QuestionsData():
 
+'''
+Class that contains all the transformations and classification functions needed for the question
+'''
+class QuestionsData():
+	
+	'''
+	Read csv that contains data and initialize the object
+	'''
 	def __init__(self,csv_path='test_data/test_data_formatted.csv',question_column_name = 'Question',output_column_name='Org_tag'):
 		self.quest_df = pd.DataFrame.from_csv(csv_path).get([question_column_name,output_column_name])
 		print '|=                                 | Loaded questions into memory'
 		
+	'''
+	Extract Syntatic features from the question
+	Syntatic Features:
+	- First Word Token : Which is the first word in the sentence ?
+	- First Word Type : Which Part-of-Speech does the first word belong to?
+	- First Noun : What is the first Noun present in the sentence ?
+	- First Question Token - What is the first question token ('Wh' word) present in the sentence ?
+	- First Question Noun - What is the first noun succeeding the first question token ?
+	- Is Affirmation - Is this question an affirmation question or not based on specific pattern rules ?
+	'''
 	def extract_syntatic_features(self):
 		quest_df = self.quest_df
 		syntatic_features = []
@@ -38,8 +55,11 @@ class QuestionsData():
 		print '|====                              | Syntatic Features Extracted'
 		self.dict_vectorize_syntatic_features(syntatic_features)
 		
-		
-				
+	'''
+	Determine if a given question is an 'affirmation' question or not
+	Returns 0 if the question is not an 'affirmation' question
+	Returns 1 if the question is an 'affirmation' question
+	'''			
 	def determine_affirmation(self,text):
 		init_string = '|'.join(be_words+mod_words)
 		be_string = '|'.join(be_words)
@@ -51,13 +71,18 @@ class QuestionsData():
 			return 0
 		return 1
 	
+
+	'''
+	Load vectorizer from pickeled model and vectorize the syntatic features
+	'''
 	def dict_vectorize_syntatic_features(self,syntatic_features):
 		dct_vect = pickle.load(open("pickled_objects/DictVectorizer/dct_vect.pkl",'rb'))
 		self.syntatic_features = dct_vect.transform(syntatic_features.T.to_dict().values())
 		print '|========                          | Syntatic Features Vectorized '
 		
-		
-		
+	'''
+	Extract uni-gram and bi-gram features from the question
+	'''	
 	def extract_ngram_features(self):
 		quest_df = self.quest_df
 		tfidf_vect = pickle.load(open("pickled_objects/TfIdfVectorizer/tfidf_vect.pkl",'rb'))
@@ -65,13 +90,19 @@ class QuestionsData():
 		print '|============                      | Ngram Features Extracted'
 		print '|=============                     | Ngram Features Vectorized'
 		
+		
+	'''
+	Merge Syntatic and N-gram feature set
+	'''
 	def merge_feature_set(self):
 		self.X = sparse.hstack((self.ngram_features,self.syntatic_features))
 		del self.syntatic_features
 		del self.ngram_features
 		print '|===================               | Final Feature Set Assembled'
 		
-		
+	'''
+	Load pickled modeled and run on feature set
+	'''
 	def classify_questions(self):
 		X = self.X
 		clf = joblib.load('pickled_objects/Models/LogistRegressionModels/logreg_complete1.pkl')
@@ -80,11 +111,19 @@ class QuestionsData():
 		del self.X
 		print '|==============================    | Questions Classified'
 		
+		
+	'''
+	Decode encoded labels to text
+	'''
 	def decode_label(self):
 		le = pickle.load(open("pickled_objects/LabelEncoder/LE.txt",'rb'))
 		self.Y_label = le.inverse_transform(self.Y)
 		print '|================================  | Label decoded'
 	
+	
+	'''
+	Get accuracy and f1 score for the data set
+	'''
 	def get_classification_staistics(self):
 		le = pickle.load(open("pickled_objects/LabelEncoder/LE.txt",'rb'))
 		quest_df =self.quest_df
@@ -102,14 +141,22 @@ class QuestionsData():
 		print confusion_matrix(Y_actual_labels,self.Y_label)
 
 		
-		
+	'''
+	Save output as a csv in 'classified_questions' folder
+	'''
 	def save_output(self):
 		Y_label = self.Y_label
 		Y_label_df = pd.DataFrame(Y_label.reshape((Y_label.shape[0],1)),columns=['Predicted_Question_Type'])
 		uuid = str(time.time())[:-4]
 		pd.concat((self.quest_df,Y_label_df),axis=1).to_csv('classified_questions/Question_Type_'+uuid+'.csv')
 		print '|==================================| Output Saved'
-		
+
+'''
+Class for getting result for individual questions
+'''
 class IndividualQuestion(QuestionsData):
+	'''
+	Initialize individual question
+	'''
 	def __init__(self,quest_string):
 		self.quest_df = pd.DataFrame([[quest_string]],columns=['Question'])
